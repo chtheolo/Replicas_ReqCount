a16023ff01a922c0278c1f62f87083a8d16a6b36
chtheolo <chtheolo@gmail.com>
2021-11-12 23:13:02 +0200

    Add config package that reads .env files.

---
+++ config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Configuration struct {
+	Service_host      string
+	Service_port      string
+	Container_db_host string
+	Container_db_port string
+}
+
+func ConfigService() (*Configuration, error) {
+	// open .env file in the local directory
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Error loading .env file!")
+		return nil, err
+	}
+
+	return &Configuration{
+		Service_host:      os.Getenv("SERVICE_HOST"),
+		Service_port:      os.Getenv("SERVICE_PORT"),
+		Container_db_host: os.Getenv("CONTAINER_DB_HOST"),
+		Container_db_port: os.Getenv("CONTAINER_DB_PORT"),
+	}, nil
+}
--- main.go
+++ main.go
@@ -4,13 +4,22 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/req_counter_service/config"
+
 	"github.com/req_counter_service/routes"
 )
 
 func main() {
+	configurations, err := config.ConfigService()
+	if err != nil {
+		fmt.Println("Error in config file")
+		panic(err)
+	}
 
-	fmt.Println("Running :: http://localhost:8083")
+	// fmt.Println("Running :: http://localhost:8083")
+	fmt.Sprintf("Running :: %s:%s ", configurations.Service_host, configurations.Service_port)
 
 	r := routes.Router()
-	fmt.Println(http.ListenAndServe(":8083", r))
+	served := fmt.Sprintf(":%s", configurations.Service_port)
+	fmt.Println(http.ListenAndServe(served, r))
 }
